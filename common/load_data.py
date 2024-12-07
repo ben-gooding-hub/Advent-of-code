10e57722433bee003a08d18125fd6df728b71bd1
Ben-gooding <benjamin.gooding@sky.uk>
2024-12-07 11:23:46 +0000

    Delete all and replace with other repo

--- common/load_data.py
+++
@@ -1,6 +0,0 @@
-def loadData(day: int, isExampleData: bool = False) -> list[str]:
-    folder = "example-data" if isExampleData else "data"
-    f = open(f"{folder}/{str(day)}", "r")
-    data = f.read().splitlines()
-    f.close()
-    return data
