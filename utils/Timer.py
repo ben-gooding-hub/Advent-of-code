1f2f33eab42a754d926c9f3e3d1412cb541325c7
Ben-windows <bregooding@gmail.com>
2024-12-29 13:52:31 +0000

    Fix timer formatter

--- utils/Timer.py
+++ utils/Timer.py
@@ -1,7 +1,7 @@
 from time import time
-def timeMethod(method, dp: int):
+def timeMethod(method, dp: int = 2):
     t0 = time()
     method()
     t1 = time()
-    format = "\{:."+ str(dp) +"f\}"
+    format = "{0:."+ str(dp) +"f}"
     print(f"Execution time: {format.format(t1-t0)}")
